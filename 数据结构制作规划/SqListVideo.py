77cba227f7fd6705c32a7d40c744c3ba1a3ed95d
AlleyNorthward <2417534000@qq.com>
2025-08-26 21:25:38 +0800

    Update SqListVideo.py

--- 数据结构制作规划/SqListVideo.py
+++ 数据结构制作规划/SqListVideo.py
@@ -1,12 +1,19 @@
 from manim import *
-from .AlgorithmBanner import AlgorithmBanner
+from pathlib import Path
+ASSETS_DIR = Path(__file__).resolve().parent.parent.parent / "assets"/"展示代码"
 
+from ..AlgorithmBanner import AlgorithmBanner
+from ..BulletedListBrace import BulletedListBrace
+from ..MyCurvedLine import MyCurvedLine
+from ..MovingCode import MovingCode
+from ..FileMobject import FileMobject
 
 class VideoText:
     def __init__(
             self,
     ):
         self.opening_animation_text()
+        self.head_file_text()
 
     def opening_animation_text(self):
         self.START_TEX = r"""
@@ -28,6 +35,31 @@ class VideoText:
             \hspace{1em}
             详见课本19页，中华传统文化中的抽象思维。
             \end{tabular}
+        """
+    def head_file_text(self):
+        self.TOTAL_TEX = [
+            r"InitList(\&L)",
+            r"DestroyList(\&L)",
+            r"ListInsert(\&L, i, e)",
+            r"ListErase(\&L, i, \&e)",
+            r"ListClear(\&L)",
+            r"ListAssign(L, i, value)",
+            r"ListEmpty(L)",
+            r"ListSize(L)",
+            r"ListGetElem(L, i \&e)",
+            r"ListFind(L, e)",
+            r"ListTraverse(L, visit())"
+        ]
+
+        self.TITLE = r"线性表\\基本操作方法"
+
+        self.CAPTION1 = r"""
+            可暂停了解,\\
+            详情见课本14页
+        """
+
+        self.CAPTION2 = r"""
+            \&
         """
 
 class SqListVideo:
@@ -81,5 +113,75 @@ class SqListVideo:
         self.banner.clear_all_mobjects(self.scene)
         self.scene.wait()
 
-        
 
+    def head_file_animation(self):
+        # Scene1
+        start_tex = BulletedListBrace(*self.total_text.TOTAL_TEX, )
+        start_tex.set(height = self.scene.camera.frame_height - 1)
+        title = start_tex.get_title(self.total_text.TITLE)
+        title.set_color(BLUE_D)
+        start_tex.add(title)
+        start_tex.shift(RIGHT*1.7)  
+        
+        self.scene.play(GrowFromCenter(title))
+        start_tex.remove(title)
+        self.scene.play(FadeIn(start_tex), run_time = 1.7)
+
+        caption1 = Tex(self.total_text.CAPTION1, tex_template = TexTemplateLibrary.ctex)
+        caption1.to_corner(UL, buff = 0.9).scale(0.7)
+        caption1.set_color(YELLOW_E)
+        cl = MyCurvedLine(start = start_tex.brace.get_center()+UP*0.1, end = caption1.get_center()+DOWN*0.8)
+
+        self.scene.play(Write(caption1), cl.create())
+        self.scene.wait(3)
+
+        self.scene.clear()
+
+        # Scene2
+        CODE_PATH = ASSETS_DIR / "0头文件.cpp"
+        define_code = MovingCode(CODE_PATH, formatter_style=MovingCode.get_cpp_stycle())
+
+        self.scene.play(FadeIn(define_code))
+        self.scene.wait(4.5)
+        self.scene.play(define_code.animate.scale(0.9).to_edge(LEFT, buff = 0.9))
+        define_code_rect = define_code.get_rect(1,3)
+        define_code_svg = define_code.get_svg(2,'F')
+
+        self.scene.wait(2)
+
+        # Scene3
+        caption2 = Tex(self.total_text.CAPTION2, tex_template = TexTemplateLibrary.ctex).scale(2)
+        caption2.shift(RIGHT*2.8+DOWN*1.3).set_color(ORANGE)
+
+        self.scene.play(Write(caption2))
+        self.scene.wait()
+
+        file_c = FileMobject().scale(0.7)
+        file_cpp = FileMobject(name = '.cpp').scale(0.7)
+        file_c.next_to(caption2, UP, buff = 1.9).shift(LEFT*1.3)
+        file_cpp.next_to(file_c, RIGHT, buff = 1.5)
+
+        file_c_curve_line = MyCurvedLine(caption2.get_center()+UP*0.3, file_c.get_center()+DOWN*0.4, points = 3)
+        file_cpp_curve_line = MyCurvedLine(caption2.get_center() + UP*0.3, file_cpp.get_center()+DOWN*0.4, points = 3)
+        self.scene.play(FadeIn(file_c))
+        self.scene.play(FadeIn(file_cpp))
+        self.scene.play(file_c_curve_line.create(), file_cpp_curve_line.create())
+
+        self.scene.wait()
+        self.scene.play(file_c.animate.set_color(GREEN_D).set_opacity(0.3))
+        self.scene.play(file_cpp.animate.set_color(RED_D))
+        self.scene.wait()
+
+        self.scene.play(FadeIn(define_code_rect), FadeIn(define_code_svg))
+        self.scene.wait(9)
+        
+        self.scene.clear()
+        code_grps = VGroup(define_code, define_code_rect, define_code_svg)
+        self.scene.add(code_grps)
+
+        self.scene.play(code_grps.animate.scale(1.1).move_to(ORIGIN))
+        self.scene.play(define_code.transform_both_svg_and_rectangle(code_grps[1], code_grps[2], 5, 8, 11))
+        self.scene.wait(4)
+
+        self.scene.play(define_code.transform_both_svg_and_rectangle(code_grps[1], code_grps[2], 13, 13))
+        self.scene.wait(11)
