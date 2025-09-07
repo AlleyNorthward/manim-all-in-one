35064b7a9ccfd963aa5d5d2fe1c40521c2c2e465
AlleyNorthward <2417534000@qq.com>
2025-09-07 20:46:02 +0800

    Update 精准移动.py

--- utils/精准移动.py
+++ utils/精准移动.py
@@ -55,3 +55,28 @@ def set_position(
     self.shift((target - move_mob) * coor_mask)
     return self
 
+
+
+# 2025.9.7日更新
+# 添加了buff, 可以更加精准地控制
+def set_position(
+        self, 
+        point_or_mobject: Point3DLike | Mobject, 
+        anchor = ORIGIN,
+        buff = 0,
+        coor_mask: Vector3D = np.array([1, 1, 1]),
+):
+    if isinstance(point_or_mobject, Mobject):
+        target = point_or_mobject.get_center()
+    else:
+        target = point_or_mobject
+        
+    move_mob = self.get_critical_point(anchor)
+
+    direction_vector = target - move_mob
+    direction_vector_length = np.linalg.norm(direction_vector)
+    unit_direction_vector = direction_vector / direction_vector_length
+    small_move_vector = unit_direction_vector * buff
+
+    self.shift((direction_vector - small_move_vector) * coor_mask)
+    return self
